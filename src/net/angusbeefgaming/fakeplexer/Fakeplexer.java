d8f074e58b954de685709b52f023f4ae3506d1b0
Atticus Zambrana <atticus.zambrana@angusbeefgaming.net>
2018-07-11 11:51:31 -0700

    Huge Updates

---
+++ src/net/angusbeefgaming/fakeplexer/Fakeplexer.java
@@ -0,0 +1,9 @@
+package net.angusbeefgaming.fakeplexer;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class Fakeplexer extends Plugin {
+	public void onEnable() {
+		
+	}
+}
---
+++ src/net/angusbeefgaming/fakeplexer/motd/MotdManager.java
@@ -0,0 +1,77 @@
+package net.angusbeefgaming.fakeplexer.motd;
+
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import net.md_5.bungee.api.event.ProxyPingEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.event.EventHandler;
+
+public class MotdManager implements Listener, Runnable
+{
+	private Plugin _plugin;
+	
+
+	private Random _random = new Random();
+	private String _firstLine = "                §b§l§m   §8§l§m[ §r §9§lInfinity§r §f§lGames§r §8§l§m ]§b§l§m   §r";
+	private List<String> _motdLines;
+	
+	public MotdManager(Plugin plugin)
+	{
+		_plugin = plugin;
+		
+		_plugin.getProxy().getScheduler().schedule(_plugin, this, 5L, 30L, TimeUnit.SECONDS);
+		_plugin.getProxy().getPluginManager().registerListener(_plugin, this);
+		
+		run();
+		
+		if (new File("updateMOTD.dat").exists())
+		{
+			List<String> lines = new ArrayList<String>();
+			lines.add("             §b§l◄§f§lNEW§b§l►     §f§l◄§b§lSKYWARS§f§l►     §b§l◄§f§lNEW§b§l►");
+			//lines.add("                     §d§lRank Sale §a§l40% Off");
+			//lines.add("                        §f§l◄§c§lMAINTENANCE§f§l►");
+			
+			updateMainMotd("                §b§l§m   §8§l§m[ §r §9§lMineplex§r §f§lGames§r §8§l§m ]§b§l§m   §r", lines);
+			System.out.println("Updated Bungee MOTD");
+		}
+	}
+ 
+	@EventHandler
+	public void serverPing(ProxyPingEvent event)
+	{
+		net.md_5.bungee.api.ServerPing serverPing = event.getResponse();
+
+		String motd = _firstLine;
+		if (_motdLines != null && _motdLines.size() > 0)
+		{
+			motd += "\n" + _motdLines.get(_random.nextInt(_motdLines.size()));
+		}
+		
+		event.setResponse(new net.md_5.bungee.api.ServerPing(serverPing.getVersion(), serverPing.getPlayers(), motd, serverPing.getFaviconObject()));
+	}
+	
+	@Override
+	public void run()
+	{
+
+	}
+	
+	/**
+	 * Update the main {@link GlobalMotd} determining the MOTD for Bungee instances.
+	 * @param motdLines - the lines to update the MOTD to.
+	 */
+	public void updateMainMotd(String headline, List<String> motdLines)
+	{
+	}
+
+	public List<String> getMotdLines()
+	{
+		return _motdLines;
+	}
+}
