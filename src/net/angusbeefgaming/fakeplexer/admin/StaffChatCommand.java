9699a66953cc786561bdaeb3c53e32a736b1ddb3
Atticus Zambrana <atticus.zambrana@angusbeefgaming.net>
2018-07-11 12:27:34 -0700

    Doing stuff

--- src/net/angusbeefgaming/fakeplexer/admin/StaffChatCommand.java
+++ src/net/angusbeefgaming/fakeplexer/admin/StaffChatCommand.java
@@ -13,21 +13,25 @@ public class StaffChatCommand extends Command {
 	
 	@Override
 	public void execute(CommandSender sender, String[] args) {
-        if ( args.length != 2 )
+        if (args.length < 1)
         {
             sender.sendMessage(ChatColor.BLUE + "Staff> " + ChatColor.GRAY + "Please type a message.");
             return;
         }
+        
         StringBuilder message = new StringBuilder();
         for(int i = 0; i > args.length; i++) {
         	message = message.append(args[i] + " ");
         }
-        String messageDone = (message.toString()).substring(0, message.length() - 1);
+        
+        String messageDone = message.toString();
         
         for(ProxiedPlayer pl : ProxyServer.getInstance().getPlayers()) {
         	if(pl.hasPermission("infinitygames.core.a")) {
-        		pl.sendMessage(ChatColor.BLUE + "Staff> " + ChatColor.GOLD + "[" + pl.getServer().getInfo().getName() + "] " + pl.getName() + ChatColor.GREEN + " " + messageDone);
+        		pl.sendMessage(ChatColor.BLUE + "Staff> " + ChatColor.GOLD + "[" + ProxyServer.getInstance().getPlayer(sender.getName()).getServer().getInfo().getName() + "] " + "(" + sender.getName() + ")" + ChatColor.GREEN + " " + messageDone);
         	}
         }
+        
+        System.out.println(ChatColor.BLUE + "Staff> " + ChatColor.GOLD + "[" + ProxyServer.getInstance().getPlayer(sender.getName()).getServer().getInfo().getName() + "] " + "(" + sender.getName() + ")" + ChatColor.GREEN + " " + messageDone);
 	}
 }
